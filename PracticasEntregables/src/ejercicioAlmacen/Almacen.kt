c11add3157c74c6f2fc2dd86f0ceb56b616277f0
Mothrax <raullg97@educastur.es>
2024-11-13 19:54:20 +0100

    Acabados algunos metodos del ejercicio (sin comprobar)

--- PracticasEntregables/src/ejercicioAlmacen/Almacen.kt
+++ PracticasEntregables/src/ejercicioAlmacen/Almacen.kt
@@ -2,16 +2,95 @@ package ejercicioAlmacen
 
 class Almacen{
 
-    fun almacen(){
-        var fila1:List<Bebida> = mutableListOf();
-        var fila2:List<Bebida> = mutableListOf();
-        var fila3:List<Bebida> = mutableListOf();
-        var fila4:List<Bebida> = mutableListOf();
-        var fila5:List<Bebida> = mutableListOf();
+        private lateinit var fila1:MutableList<Bebida?>;
+        private lateinit var fila2:MutableList<Bebida?>;
+        private lateinit var fila3:MutableList<Bebida?>;
+        private lateinit var fila4:MutableList<Bebida?>;
+        private lateinit var fila5:MutableList<Bebida?>;
+        private lateinit var estanteria: MutableList<MutableList<Bebida?>>
 
-        //La fila1 es la primera de todas (La que mas abajo está)
+        fun constructor(){
+            fila1 = MutableList(5) { null }
+            fila2 = MutableList(5) { null }
+            fila3 = MutableList(5) { null }
+            fila4 = MutableList(5) { null }
+            fila5 = MutableList(5) { null }
+            estanteria = arrayListOf(fila1, fila2, fila3, fila4, fila5);
+        }
 
-        var estanteria = arrayOf(fila1, fila2, fila3, fila4, fila5);
+
+    fun agregarBebida(Bebida:Bebida){
+        var bebidaRepe = false;
+
+        for (i in estanteria.indices){
+            for (j in estanteria[i]){
+                if(j != null){
+                    if (j.id == Bebida.id) {
+                        bebidaRepe = true;
+                    }
+                }
+            }
+        }
+
+        if(!bebidaRepe){
+            for (i in estanteria.indices){
+                for (j in estanteria[i]){
+                    if (j == null) {
+                        estanteria[i].add(Bebida)
+                    }
+                }
+            }
+        }
+    }
+
+    fun eliminarBebida(id:Long){
+        for (i in estanteria.indices){
+            for (j in estanteria[i]){
+                if (j != null) {
+                    if (j.id == id) {
+                        estanteria[i].removeAt(i)
+                    }
+                }
+            }
+        }
+    }
+    //Calcular precio TOTAL
+    fun calcularPrecio (){
+        var precioTotal:Double = 0.0;
+        for (i in estanteria.indices){
+            for (j in estanteria[i]){
+                if (j != null) {
+                    precioTotal =+ j.precio
+                }
+            }
+        }
+        println("El precio total de todas las bebidas de la estanteria es de $precioTotal")
+    }
+
+    //Calcular precio por columna
+    fun calcularPrecio (columna:Int){
+        var precioTotalColumna:Double = 0.0;
+        for (i in estanteria.indices){
+            for (j in estanteria[i]){
+                if (j != null) {
+                    precioTotalColumna =+ j.precio
+                }
+            }
+        }
+        println("El precio total de la columna $columna es de $precioTotalColumna")
+    }
+
+
+    fun mostrarBebidas(){
+        for (i in estanteria.indices){
+            for (j in estanteria[i]){
+                if (j != null) {
+                    println("---------- Datos de la bebida ----------")
+                    println(j)
+
+                }
+            }
+        }
     }
 
 
